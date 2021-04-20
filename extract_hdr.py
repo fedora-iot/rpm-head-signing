08b58b36b46fc4278b349ee88dbad705a71705fc
Patrick Uiterwijk <patrick@puiterwijk.org>
2021-04-20 11:18:40 +0200

    Make python scripts reusable
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- extract_hdr.py
+++ extract_hdr.py
@@ -3,27 +3,34 @@ import rpm
 from koji import (get_rpm_header, rpm_hdr_size, find_rpm_sighdr, RPM_TAG_FILEDIGESTALGO, RPM_FILEDIGESTALGO_IDS)
 import sys
 
-if len(sys.argv) != 4:
-    raise Exception("Call: %s <input-rpm> <output-hdr> <output-digests>" % sys.argv[0])
 
-path = sys.argv[1]
-hdr_out = sys.argv[2]
-digests_out = sys.argv[3]
+def extract_header(input_path, header_out_path, digest_out_path):
+    (sig_start, sig_size) = find_rpm_sighdr(input_path)
+    hdr_start = sig_start + sig_size
+    hdr_size = rpm_hdr_size(input_path, hdr_start)
 
-(sig_start, sig_size) = find_rpm_sighdr(path)
-hdr_start = sig_start + sig_size
-hdr_size = rpm_hdr_size(path, hdr_start)
+    rpm_hdr = get_rpm_header(input_path)
+    file_digestalgo = rpm_hdr[RPM_TAG_FILEDIGESTALGO]
+    file_digestalgo = RPM_FILEDIGESTALGO_IDS[file_digestalgo].lower()
+    file_digests = set([x.upper() for x in rpm_hdr[rpm.RPMTAG_FILEDIGESTS]])
 
-rpm_hdr = get_rpm_header(path)
-file_digestalgo = rpm_hdr[RPM_TAG_FILEDIGESTALGO]
-file_digestalgo = RPM_FILEDIGESTALGO_IDS[file_digestalgo].lower()
-file_digests = set([x.upper() for x in rpm_hdr[rpm.RPMTAG_FILEDIGESTS]])
+    with open(digest_out_path, "at") as df:
+        for digest in file_digests:
+            digest = digest.strip()
+            if not digest:
+                continue
+            df.write("%s:%s\n" % (file_digestalgo, digest))
 
-with open(digests_out, "wt") as df:
-    for digest in file_digests:
-        df.write("%s:%s\n" % (file_digestalgo, digest))
+    with open(input_path, "rb") as f:
+        f.seek(hdr_start)
+        with open(header_out_path, "wb") as of:
+            of.write(f.read(hdr_size))
 
-with open(path, "rb") as f:
-    f.seek(hdr_start)
-    with open(hdr_out, "wb") as of:
-        of.write(f.read(hdr_size))
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 4:
+        raise Exception("Call: %s <input-rpm> <output-hdr> <output-digests>" % sys.argv[0])
+
+    extract_header(sys.argv[1], sys.argv[2], sys.argv[3])
--- insert_signature.py
+++ insert_signature.py
@@ -1,33 +1,43 @@
 #!/usr/bin/env python
 import subprocess
-import sys
 
-if len(sys.argv) not in (4, 5):
-    raise Exception("Call: %s <ima_lookup.so-path> <rpm-path> <header-signature> [ima_presigned_directory]")
 
-ima_lookup_path = sys.argv[1]
-rpm_path = sys.argv[2]
-sig_path = sys.argv[3]
-if len(sys.argv) == 3:
-    ima_args = []
-else:
-    ima_args = [
-        '--define',
-        '_file_signing_key %s' % sys.argv[4],
-        '--signfiles',
-    ]
+def insert_signature(rpm_path, sig_path, ima_lookup_path=None, ima_presigned_path=None):
+    if ima_presigned_path is None:
+        ima_args = []
+    else:
+        ima_args = [
+            '--define',
+            '_file_signing_key %s' % ima_presigned_path,
+            '--signfiles',
+        ]
 
-subprocess.check_call(
-    [
-        'rpmsign',
-        '--define',
-        '_gpg_name fakename',
-        '--define',
-        '__gpg_sign_cmd /usr/bin/cp cp "%s" "%%{__signature_filename}"' % sig_path,
-        '--addsign',
-        rpm_path,
-    ] + ima_args,
-    env={
-        'LD_PRELOAD': ima_lookup_path,
-    },
-)
+    subprocess.check_call(
+        [
+            'rpmsign',
+            '--define',
+            '_gpg_name fakename',
+            '--define',
+            '__gpg_sign_cmd /usr/bin/cp cp "%s" "%%{__signature_filename}"' % sig_path,
+            '--addsign',
+            rpm_path,
+        ] + ima_args,
+        env={
+            'LD_PRELOAD': ima_lookup_path,
+        },
+    )
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) == 3:
+        ima_lookup_path = None
+        ima_presigned_path = None
+    elif len(sys.argv) == 5:
+        ima_lookup_path = sys.argv[3]
+        ima_presigned_path = sys.argv[4]
+    else:
+        raise Exception("Call: %s <rpm-path> <header-signature> [ima_lookup.so_path] [ima_presigned_directory]")
+
+    insert_signature(sys.argv[1], sys.argv[2], ima_lookup_path, ima_presigned_path)
